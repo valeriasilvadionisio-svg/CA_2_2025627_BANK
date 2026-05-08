160222cbc3776b737d60ad16dd4ebb528d6b9110
Valéria Dionisio <valeriasilvadionisio@gmail.com>
2026-05-08 15:12:01 +0100

    first commit created a code

---
+++ netbeans /CA_2/src/main/java/CA_2/CA_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package CA_2;
+
+/**
+ * @author valeriadionisio
+ */
+//CA2 Integrated Project - I chosed Bank Organisation 
+// The program will allow the user to:
+//read employee/applicant records from a file;
+//sort records alphabetically;
+//search for a person and show their manager type and department;
+//add new employee records with input validation;
+//create an employee hierarchy using a binary tree.
+//The system will use the package name CA_2 as required in the assignment brief.
+
+public class CA_2 {
+
+   
+    public static void main(String[] args) {
+
+       
+        System.out.println("Welcome to the Bank Organisation System");
+
+    }
+}
