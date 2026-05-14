4fd1f799d63cf74356e340255493736b7b08218e
Valéria Dionisio <valeriasilvadionisio@gmail.com>
2026-05-14 06:08:47 +0100

    Add menu option enum

---
+++ netbeans /CA_2/src/main/java/CA_2/MenuOption.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author valeriadionisio
+ */
+
+//I am using an enum because the menu has fixed options, and this helps avoid using random text values in the program.
+//Enum created to organise the main menu options.
+
+public enum MenuOption {
+    READ_FILE,
+    SORT,
+    SEARCH,
+    ADD_RECORD,
+    CREATE_BINARY_TREE,
+    EXIT
+    
+}
